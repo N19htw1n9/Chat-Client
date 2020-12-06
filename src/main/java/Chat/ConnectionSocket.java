7327c21404c2799b76c9e2bf2f676d292dbd9233
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2020-12-06 17:33:45 -0600

    Update ConnectionSocket

--- src/main/java/Chat/ConnectionSocket.java
+++ src/main/java/Chat/ConnectionSocket.java
@@ -6,20 +6,23 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.Socket;
 import java.lang.Thread;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ConnectionSocket extends Thread {
     private Socket socket;
     private String ip;
     private int port;
-    private Consumer<Serializable> callback;
+    private Consumer<Serializable> chatCallback, clientsListCallback;
     private ObjectOutputStream out;
     private ObjectInputStream in;
 
-    public ConnectionSocket(Consumer<Serializable> callback, String ip, int port) {
+    public ConnectionSocket(Consumer<Serializable> chatCallback, Consumer<Serializable> clientsListCallback, String ip,
+            int port) {
         this.ip = ip;
         this.port = port;
-        this.callback = callback;
+        this.chatCallback = chatCallback;
+        this.clientsListCallback = clientsListCallback;
     }
 
     @Override
@@ -29,19 +32,36 @@ public class ConnectionSocket extends Thread {
             out = new ObjectOutputStream(this.socket.getOutputStream());
             in = new ObjectInputStream(this.socket.getInputStream());
             this.socket.setTcpNoDelay(true);
-        } catch (IOException e) {
-            System.out.printf("\n\nError, could not connect to server %s port %d\n", this.ip, this.port);
+
+            chatCallback.accept("Connected to server...");
+
+            while (true) {
+                ChatData res = (ChatData) in.readObject();
+
+                if (res.clients != null) {
+                    clientsListCallback.accept((Serializable) res.clients);
+                }
+                if (res.message != null) {
+                    chatCallback.accept(
+                            String.format("From: %d;\t To: %d\nMessage: %s", res.from.id, res.to.id, res.message));
+                }
+            }
+        } catch (Exception e) {
+            chatCallback.accept(String.format("Error, could not connect to server %s port %d", this.ip, this.port));
         }
-        System.out.printf("\nConnected to %s server on port %d\n", this.socket.getLocalSocketAddress().toString(),
-                this.socket.getLocalPort());
+    }
+
+    private ChatData.ChatUser buildUser(int id, String name) {
+        ChatData.ChatUser user = new ChatData.ChatUser();
+        user.id = id;
+        user.name = name;
+        return user;
     }
 
     public void send(String message, int to) throws IOException, ClassNotFoundException {
-        Packet req = new Packet();
+        ChatData req = new ChatData();
+        req.message = message;
+        req.to = buildUser(to, null);
         out.writeObject(req);
-    }
-
-    public Packet recieve() throws IOException, ClassNotFoundException {
-        return (Packet) in.readObject();
     }
 }
